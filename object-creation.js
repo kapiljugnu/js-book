a9b667c178feb0b433e42469dcffe438b785fbe9
Kapil Jugnu <kr_kapil@hotmail.com>
2019-04-10 12:24:24 +0530

    object creation

---
+++ object-creation.js
@@ -0,0 +1,24 @@
+/*
+* 
+* Object is fundamental datatype in javascript.
+* It is the only complex datatype.
+* Rest datatype are primitive (number, boolean, string, symbol, null, undefined).
+* Object are mutable (can be changed).
+* Object is unordered list of primitive datatype,
+* that is stored as a series of name-value pair.
+* Everything in js is object.
+*
+*/
+
+
+// object literal
+
+// Object constructor
+
+// object.create()
+
+// es6 classes
+
+// constructor function pattern
+
+// prototype pattern
