dcfadcceb26232012fd81149e3e2bdfcb654b39f
Kapil Jugnu <kr_kapil@hotmail.com>
2019-04-25 07:42:36 +0530

    reverse array rotation

--- programs/array/rotation.js
+++ programs/array/rotation.js
@@ -13,7 +13,7 @@ function usingTemp(arr, degree) {
 // O(n*d)
 function shiftingOneByOne(arr, degree) {
   for (let i = 0; i < degree; i++) {
-    const temp = arr[i];
+    const temp = arr[0];
     let j = 0;
     for (; j < arr.length - 1; j++) {
       arr[j] = arr[j + 1];
@@ -23,5 +23,27 @@ function shiftingOneByOne(arr, degree) {
   return arr;
 }
 
-console.log(usingTemp([1, 2, 3, 4, 5, 6, 7], 2));
-console.log(shiftingOneByOne([1, 2, 3, 4, 5, 6, 7], 2));
+/*
+Time Complexity : O(n)
+*/
+function usingReverseArray(arr, degree) {
+  function reverse(start, end) {
+    while (start < end) {
+      const temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  reverse(0, degree - 1);
+  reverse(degree, arr.length - 1);
+  reverse(0, arr.length - 1);
+  return arr;
+}
+
+const list = [1, 2, 3, 4, 5, 6, 7],
+  degree = 2;
+console.log(`usingTemp=>`, usingTemp([...list], degree));
+console.log(`shiftingOneByOne=>`,shiftingOneByOne([...list], degree));
+console.log(`usingReverseArray=>`,usingReverseArray([...list], degree));
